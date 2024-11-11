30930056ed5851c11b13e5eac3343bcb93cbb2e8
BenWeisz <bencew99@gmail.com>
2024-11-11 15:45:28 -0800

    Remove all files except README in main branch

--- include/verify.h
+++
@@ -1,9 +0,0 @@
-#ifndef CJSON_VERIFY_H
-#define CJSON_VERIFY_H
-
-#include "types.h"
-#include "log.h"
-
-unsigned int CJSON_verify_scopes( CJSON_TOKEN* tokens, unsigned int num_tokens );
-
-#endif // CJSON_VERIFY_H
